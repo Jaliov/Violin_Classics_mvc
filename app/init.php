972a10affded696fa377b886ecfd719e3c27d61a
Joel Rudin <jarud55@gmail.com>
2022-05-28 19:20:57 -0400

    form db tweaks

--- app/init.php
+++ app/init.php
@@ -3,6 +3,6 @@ require_once "../app/core/app.php";
 require_once "../app/core/functions.php";
 require_once "../app/core/controller.php";
 require_once "../app/core/db_connect.php";
-require_once "../app/core/form_db.php";
+// require_once "../app/core/form_db.php";
 // require_once "../app/core/index.php";
 ?>
