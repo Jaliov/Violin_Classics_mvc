aab63e48e26b555b33b797f48019abf99ed1ef54
Joel Rudin <jarud55@gmail.com>
2022-05-29 10:45:12 -0400

    back to 4

--- app/views/header.php
+++ app/views/header.php
@@ -4,21 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
     <link rel="icon" href="./assets/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href='./assets/css/style.css'>
-
+    
     <title>Great Violinists Past & Present Play the Classics</title>
-    <meta name="description" content="An historical overview of great violin concertos as played by great violinists, past and present">
+    <meta name = "description" content="An historical overview of great violin concertos as played by great violinists, past and present"> 
     <meta name=”robots” content="index, follow">
 </head>
 
 <body>
     <?php require 'navbar.php'; ?>
-
+   
     <div class="container container-fluid">
         <div class="card text-center bg-transparent">
 
