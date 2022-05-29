abe517253739e3b54a319fd9d71c63802f6142c4
Joel Rudin <jarud55@gmail.com>
2022-05-29 11:00:32 -0400

    form edits

--- app/views/contact.php
+++ app/views/contact.php
@@ -5,22 +5,24 @@ $this->view("header", $data);
 <?php require '../app/core/form_db.php'; ?>
 
 <div class = 'container'>
-    <form method = 'POST' action = 'contact'>
-    <div class="row">
-    <div class="col">
+<div class='form-group'> 
+<form method = 'POST' action = 'contact'>
    <span class = 'text-danger' id='reset'><?=$formSubmit?></submit>
- 
+    </div>
+    <div class = "row">
+    <div class="col">
          <label for='name_entry' class ='text-light'>Name</label>
          <input type='text' name = 'name_entry' class='form-control' placeholder = 'Enter name'><span class = 'text-danger'>
          <?=$NameError?> </span>
      </div>
-     <div class="col">
-                 <div class='form-group'> 
+   
+    
+               
                    <div class="col">
                  <label for='email' class ='text-light'>Email</label>
                  <input type='email' class='form-control' name ='email' placeholder='name@youremail.tldomain'> 
                </div>
-               
+               </div>
                <div class='form-group'>
                  <label for='comments' class ='text-light'>Comments</label>
                  <textarea class='form-control' name='comments' rows='3' placeholder='Violinistic comments...'></textarea><br>
