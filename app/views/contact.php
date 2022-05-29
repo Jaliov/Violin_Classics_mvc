0b0814f6806f2dab408e6c81911e5b1aff02364f
Joel Rudin <jarud55@gmail.com>
2022-05-29 10:54:12 -0400

    form modify

--- app/views/contact.php
+++ app/views/contact.php
@@ -6,12 +6,17 @@ $this->view("header", $data);
 
 <div class = 'container'>
     <form method = 'POST' action = 'contact'>
+    <div class="row">
+    <div class="col">
    <span class = 'text-danger' id='reset'><?=$formSubmit?></submit>
-    </div>
+ 
          <label for='name_entry' class ='text-light'>Name</label>
          <input type='text' name = 'name_entry' class='form-control' placeholder = 'Enter name'><span class = 'text-danger'>
          <?=$NameError?> </span>
-                 <div class='form-group'>
+     </div>
+     <div class="col">
+                 <div class='form-group'> 
+                   <div class="col">
                  <label for='email' class ='text-light'>Email</label>
                  <input type='email' class='form-control' name ='email' placeholder='name@youremail.tldomain'> 
                </div>
@@ -20,7 +25,8 @@ $this->view("header", $data);
                  <label for='comments' class ='text-light'>Comments</label>
                  <textarea class='form-control' name='comments' rows='3' placeholder='Violinistic comments...'></textarea><br>
                <button class ='btn-outline-danger btn-light btn-lg' type = 'submit' name='submit'>Submit</button>
-               
+               </div>
+  </div>
         </form>
         <script>
         setTimeout(() => {
