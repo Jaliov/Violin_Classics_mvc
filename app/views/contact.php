e7611298a872515f8aadc7fbd8c312eaa2a8639f
Joel Rudin <jarud55@gmail.com>
2022-05-29 11:05:42 -0400

    style tweaks

--- app/views/contact.php
+++ app/views/contact.php
@@ -4,37 +4,35 @@ $this->view("header", $data);
 <?php require '../app/core/db_connect.php'; ?>
 <?php require '../app/core/form_db.php'; ?>
 
-<div class = 'container'>
-<div class='form-group'> 
-<form method = 'POST' action = 'contact'>
-   <span class = 'text-danger' id='reset'><?=$formSubmit?></submit>
-    </div>
-    <div class = "row">
-    <div class="col">
-         <label for='name_entry' class ='text-light'>Name</label>
-         <input type='text' name = 'name_entry' class='form-control' placeholder = 'Enter name'><span class = 'text-danger'>
-         <?=$NameError?> </span>
-     </div>
-   
-    
-               
-                   <div class="col">
-                 <label for='email' class ='text-light'>Email</label>
-                 <input type='email' class='form-control' name ='email' placeholder='name@youremail.tldomain'> 
-               </div>
-               </div>
-               <div class='form-group'>
-                 <label for='comments' class ='text-light'>Comments</label>
-                 <textarea class='form-control' name='comments' rows='3' placeholder='Violinistic comments...'></textarea><br>
-               <button class ='btn-outline-danger btn-light btn-lg' type = 'submit' name='submit'>Submit</button>
-               </div>
+<div class='container'>
+  <div class='form-group'>
+    <form method='POST' action='contact'>
+      <span class='text-danger' id='reset'><?= $formSubmit ?></submit>
   </div>
-        </form>
-        <script>
-        setTimeout(() => {
-            document.querySelector('#reset').innerHTML = ''
-        }, 3000);
-        </script>
-        </div>
+  <div class="row">
+    <div class="col">
+      <label for='name_entry' class='text-light'>Name</label>
+      <input type='text' name='name_entry' class='form-control' placeholder='Enter name'><span class='text-danger'>
+        <?= $NameError ?> </span>
+    </div>
 
-<?php $this->view("footer",$data);?>
+    <div class="col">
+      <label for='email' class='text-light'>Email</label>
+      <input type='email' class='form-control' name='email' placeholder='name@youremail.tldomain'>
+    </div>
+  </div>
+  <div class='form-group'>
+    <label for='comments' class='text-light'>Comments</label>
+    <textarea class='form-control' name='comments' rows='3' placeholder='Violinistic comments...'></textarea><br>
+    <button class='btn-outline-danger btn-light btn-lg' type='submit' name='submit'>Submit</button>
+  </div>
+</div>
+</form>
+<script>
+  setTimeout(() => {
+    document.querySelector('#reset').innerHTML = ''
+  }, 3000);
+</script>
+</div>
+
+<?php $this->view("footer", $data); ?>
--- app/views/links.php
+++ app/views/links.php
@@ -8,15 +8,13 @@ $this->view("header", $data);
         <div class="card-title">Other sites I built:</div>
 
         <div class="text-center">
-      
-        <a href="https://lisabflamenco.herokuapp.com/" class="btn btn-outline-danger btn-light btn-list"
-                target="_blank">Lisa Botalico Flamenco!</a>
-                
-        <a href="https://jaliov.github.io/JoelRudinArt/index.html" class="btn btn-outline-danger btn-light btn-list"  target="_blank">Joel Rudin Art</a>
 
-        <div class="card-title">Also:</div>    
-        <a href="https://soundwavecompany.com/products/birds-out-my-window" class="btn btn-outline-danger btn-light btn-list" target="_blank">A piece I composed</a>           
-           
+            <a href="https://lisabflamenco.herokuapp.com/" class="btn btn-outline-danger btn-light btn-list" target="_blank">Lisa Botalico Flamenco!</a>
+
+            <a href="https://jaliov.github.io/JoelRudinArt/index.html" class="btn btn-outline-danger btn-light btn-list" target="_blank">Joel Rudin Art</a>
+
+            <a href="https://soundwavecompany.com/products/birds-out-my-window" class="btn btn-outline-danger btn-light btn-list" target="_blank">A piece I composed</a>
+
         </div>
     </div>
 </div>
