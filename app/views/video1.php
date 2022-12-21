d19cce9597139bc74ae89b55156be4bcadaf5a6a
Joel Rudin <jarud55@gmail.com>
2022-12-21 11:36:36 -0500

    add more classes

--- app/views/video1.php
+++ app/views/video1.php
@@ -5,8 +5,11 @@ require 'video_list.php';
 require 'views.php';
 require './assets/js/popover_script.php';
 
-echo $heading;
+$stmt = $connection->prepare('SELECT * FROM musicians WHERE id = ?');
+$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
+$paganini = 'Niccolò Paganini';
 
+echo $heading;
 class View
 {
     public $view;
@@ -15,7 +18,6 @@ class View
     public $connection;
     public $role;
     public $info;
-    public $spacer;
     public function get_info()
     {
         require 'HTML.php';
@@ -57,6 +59,45 @@ class View
         $this->info = $info;
     }
 }
+
+class Paganini extends View
+{
+    public function showPaganini()
+    {
+        $paganini = 'Niccolò Paganini';
+        require 'HTML.php';
+        echo $this->view;
+        $this->id;
+        $stmt = $this->connection->prepare('SELECT * FROM musicians WHERE id = ?');
+
+        $stmt->execute(array(12));
+        $musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
+        foreach ($musicians as $musician) :
+            echo $buttonPlace . "title = '$musician->role'
+        data-content= '$paganini $musician->info' >$paganini</button>";
+        endforeach;
+        echo "</div></div>";
+    }
+}
+class Mozart extends View
+{
+    function justMozart()
+    {
+        require 'HTML.php';
+        $stmt = $this->connection->prepare('SELECT * FROM musicians WHERE id = ?');
+        $stmt->execute(array(6));
+        $musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
+        foreach ($musicians as $musician) :
+            echo $buttonPlace . "title = '$musician->role'
+        title = '$musician->role'
+        data-content= '$musician->info' >Mozart</button>";
+        endforeach;
+        foreach ($musicians as $musician) : ?>
+            <div style="margin-top:-20px;"><a class='btn btn-outline-danger btn-sm text-light' id="violaReset" href='https://frozen-beach-43331.herokuapp.com/' target='_blank'>Test your knowledge of the viola here!</a></div>"; ?>
+        <?php endforeach; 
+        echo "</div></div><br>"; 
+    }
+}
 $view_1 = new View($view1, 20, $connection,  $info1);
 $view_1->get_info(); //Szeryng
 $view_1 = new View('', 19, $connection, '',);
@@ -86,177 +127,46 @@ $view_5 = new View($view11, 21, $connection,  $info5);
 $view_5->get_info();
 $view_5 = new View('', 22, $connection, '');
 $view_5->get_composer();
-$view_5 = new View('', 6, $connection, '');
-$id = 6;
-$stmt = $connection->prepare('SELECT * FROM musicians WHERE id = ?'); //just Mozart
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-
-foreach ($musicians as $musician) : 
-echo $buttonPlace . "title = '$musician->role'
-title = '$musician->role'
-data-content= '$musician->info' ?>Mozart</button>"; 
-endforeach;
-$stmt->execute(array($id)); $musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-
-?>
-
-<?php foreach($musicians as $musician): ?>
-<div style="margin-top:-20px;"><a class = 'btn btn-outline-danger btn-sm text-light'  id = "violaReset"  href='https://frozen-beach-43331.herokuapp.com/'  target='_blank'>Test your knowledge of the viola here!</a></div>"; ?>
-<?php endforeach; echo "</div></div><br>";
+$view_5 = new Mozart('', 6, $connection, '');
+$view_5->justMozart();
 
 $view_6 = new View($view5, 13, $connection,  $info6);
-$view_6->get_info(); //Perlaman
+$view_6->get_info(); //Perlman
 $view_6 = new View('', 14, $connection, '');
 $view_6->get_composer();
 $view_6->spacer();
+
+$view_7 = new View($view6, 11, $connection,  $info7);
+$view_7->get_info();  //Ricci
+$view_7 = new Paganini('', 12, $connection, '');
+$view_7->showPaganini();
+
+$view_8 = new View($view7, 9, $connection,  $info8);
+$view_8->get_info();  //Mutter
+$view_8 = new View('', 10, $connection, '');
+$view_8->get_composer();  //Mendelssohn
+$view_8->spacer();
+
+$view_9 = new View($view8, 15, $connection,  $info9);
+$view_9->get_info(); 
+$view_9 = new View('', 16, $connection, '');
+$view_9->get_composer();
+$view_9->spacer();
+
+$view_10 = new View($view9, 1, $connection,  $info10);
+$view_10->get_info(); //Heifetz
+$view_10 = new View('', 17, $connection, '');
+$view_10->get_composer();  //Tchaikovsky
 ?>
+</button><br/><a class = 'btn btn-outline-danger btn-sm text-light' href='https://jaliov.github.io/react-heifetz/' target='_blank' id='heifetz_link'>Check out my Heifetz Tribute Page!</a>
+</div></div><br>
 
-<?php echo $view6; ?>
-<!-- Paganini -->
-<?php $id = 11;
-$stmt;
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-
-$paganini = 'Niccolò Paganini'; ?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $info7 . $musician->concerto .
-        "</span></div></div>"; ?>
-    <?php echo $buttonPlace . "title = '$musician->role' data-content=   '$musician->info'>$musician->name</button>"; ?>
-<?php endforeach; ?>
-
-<?php $id = 12;
-$stmt;
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
+<?php
+$view_11 = new View($view10, 8, $connection,  $info11);
+$view_11->get_info();  //Ferras
+$view_11 = new View('', 18, $connection, '');
+$view_11->get_composer(); //Sibelius
+$view_11->spacer();
 ?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $buttonPlace . "title = '$musician->role'
-data-content= '$paganini $musician->info' ?>$paganini</button>"; ?>
-<?php endforeach;
-echo "</div></div>";
-
-?>
-<?php echo $view7; ?>
-<!-- Mutter Mendelsohn -->
-
-<?php $id = 9;
-$stmt;
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $info8 . $musician->concerto .
-        "</span></div></div>"; ?>
-    <?php echo $buttonPlace .
-        "data-content= '$musician->info' ?>$musician->name</button>"; ?>
-<?php endforeach;
-?>
-
-<?php $id = 10;
-$stmt;
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $buttonPlace . "title = '$musician->role'
-data-content= '$musician->info' ?>$musician->name</button>"; ?>
-<?php endforeach;
-echo "</div></div>";
-?>
-
-<?php echo $view8; ?>
-<!-- Midori Brahms -->
-
-<?php $id = 15;
-$stmt;
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $info9 . $musician->concerto .
-        "</span></div></div>"; ?>
-    <?php echo $buttonPlace .
-        "title = '$musician->role'
- data-content= '$musician->info' >$musician->name</button>"; ?>
-<?php endforeach; ?>
-
-<?php $id = 16;
-$stmt;
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $buttonPlace . "title = '$musician->role'
- data-content= '$musician->info' >$musician->name</button>"; ?>
-<?php endforeach;
-echo "</div></div>";
-?>
-
-<?php echo $view9; ?>
-<!--Heifetz Tchaikovsky-->
-<?php $id = 1;
-$stmt;
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $info10 . $musician->concerto .
-        "</span></div></div>"; ?>
-    <?php echo $buttonPlace . "title = '$musician->role'
-data-content= '$musician->info' ?>$musician->name</button>"; ?>
-<?php endforeach; ?>
-
-<?php $id = 17;
-$stmt;
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $buttonPlace . "title = '$musician->role'
-data-content= '$musician->info' ?>$musician->name</button><br/><a class = 'btn btn-outline-danger btn-sm text-light' href='https://jaliov.github.io/react-heifetz/' target='_blank' id='heifetz_link'>Check out my Heifetz Tribute Page!</a>"; ?>
-<?php endforeach;
-echo "</div></div><br>"; ?>
-
-
-<?php echo $view10; #Ferras Sibelius
-?>
-
-<?php $id = 8;
-$stmt;
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $info11 . $musician->concerto .
-        "</span></div></div>"; ?>
-    <?php echo $buttonPlace . "title = '$musician->role'
-data-content= '$musician->info' ?>$musician->name</button>"; ?>
-<?php endforeach;
-?>
-
-<?php $id = 18;
-$stmt; //Sibelius
-$stmt->execute(array($id));
-$musicians = $stmt->fetchAll(PDO::FETCH_OBJ);
-?>
-
-<?php foreach ($musicians as $musician) : ?>
-    <?php echo $buttonPlace . "title = '$musician->role'
-title = '$musician->role'
-data-content= '$musician->info' ?>$musician->name</button>"; ?>
-<?php endforeach;
-echo "</div></div><br>"; ?>
-
 </div>
 </div>
